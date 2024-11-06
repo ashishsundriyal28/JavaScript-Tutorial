221ca0cfcec59da956ff361c6131cf24bdf2b37c
Ashish Sundriyal <ashishsundriyal@cdac.in>
2024-11-06 18:23:55 +0530

    Lecture-01 Basics

---
+++ 01_Basics/02/04.js
@@ -0,0 +1,12 @@
+// let keyword
+// declare variable with let keyword
+
+// ********** DIFF BETWEEN VAR Vs LET ************
+// block scope vs function scope
+// (covered later in this video)
+
+
+// let firstName = "Ashish" ;
+// let firstName = "Mohit" ;
+// console.log(firstName);
+// SyntaxError: Identifier 'firstName' has already been declared
---
+++ 01_Basics/02/05.js
@@ -0,0 +1,5 @@
+// const
+// declare constants
+
+const pi = 3.14 ;
+console.log(pi);
