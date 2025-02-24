158e714dbb8ba816fb49170845832d2101ab58f9
Ashish Sundriyal <ashishsundriyal@cdac.in>
2025-02-24 17:13:52 +0530

    closures

--- 08_Closures/04.js
+++ 08_Closures/04.js
@@ -1,1 +1,23 @@
 // ---------- CLOSURE EXAMPLE-3 -----------------
+function func() {
+    let counter = 0;
+    return function () {
+        if (counter < 1) {
+            console.log("Hi, you called me !!");
+            counter++;
+        }
+        else {
+            console.log("Already called one time !!");
+        }
+    }
+}
+
+const myFunc = func();
+myFunc();
+myFunc();
+myFunc();
+
+const myFunc2 = func();
+myFunc2();
+myFunc2();
+myFunc2();
