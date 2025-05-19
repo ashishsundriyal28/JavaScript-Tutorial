a8ed533c1eef020a6403030ec286c9c60bd98e65
Ashish Sundriyal <ashishsundriyal028@gmail.com>
2025-05-19 23:20:28 +0530

    minor update

--- 09_DOM/01.js
+++ 09_DOM/01.js
@@ -1,13 +1,10 @@
 // DOM = Document Object Model
 
+// HTML representation of the document
+// console.log(window.document);
+
+// JavaScript representation of the document
 // console.dir(window.document);
 
-
-
-
-
-
-
-
-
-
+// also can be written just as
+// console.dir(document);
--- 09_DOM/03.js
+++ 09_DOM/03.js
@@ -1,8 +1,11 @@
 // select element using query selector
-
+// # for id, . for class
 const mainHeading = document.querySelector("#main-heading");
 const header = document.querySelector(".header");
+
+// this will give the first occurance of class *nav-item*
 const navItem = document.querySelector(".nav-item");
+// this will give all the occurance of class *nav-item* (querySelectorAll)
 const navItemAll = document.querySelectorAll(".nav-item");
 // console.log(mainHeading);
 // console.log(header);
